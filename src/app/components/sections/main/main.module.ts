462f00275e596b00c67f555bdda79c2bebdb4db5
YenyGa <garcia.yeny@hotmail.com>
2020-02-09 17:47:42 -0500

    Search funtionality

--- src/app/components/sections/main/main.module.ts
+++ src/app/components/sections/main/main.module.ts
@@ -3,12 +3,14 @@ import {CommonModule} from '@angular/common';
 import {PokemonListComponent} from './pokemon-list/pokemon-list.component';
 import { PokemonCardComponent } from './pokemon-list/pokemon-card/pokemon-card.component';
 import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [PokemonListComponent, PokemonCardComponent, PokemonDetailsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class MainModule { }
--- src/app/components/sections/main/pokemon-list/pokemon-list.component.ts
+++ src/app/components/sections/main/pokemon-list/pokemon-list.component.ts
@@ -5,6 +5,7 @@ import {Actions, ofType} from '@ngrx/effects';
 import {take} from 'rxjs/operators';
 import {State} from '../../../../core/store';
 import {PokemonModel} from '../../../../core/models/pokemon.model';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -14,18 +15,43 @@ import {PokemonModel} from '../../../../core/models/pokemon.model';
 export class PokemonListComponent implements OnInit {
 
   pokemonList: PokemonModel[];
+  filteredList: PokemonModel[];
+  searchControl: FormControl;
 
   constructor(private store: Store<State>,
               private actions$: Actions) { }
 
   ngOnInit(): void {
+    this.searchControl = new FormControl('');
+    this.searchControl.valueChanges.subscribe(value => {
+      this.filteredList = this.filterList(value);
+    });
     this.actions$.pipe(
       ofType<PokemonListLoaded>(PokemonActionTypes.PokemonListLoaded),
       take(1)
     ).subscribe(({payload}) => {
       this.pokemonList = payload.pokemonList;
+      this.filteredList = payload.pokemonList;
     });
     this.store.dispatch(new LoadPokemonList());
+  }
+
+  private filterList(searchKey: string): PokemonModel[] {
+    const filteredList: PokemonModel[] = [];
+    this.pokemonList.forEach(pokemon => {
+      if (pokemon.name.search(searchKey) >= 0 || this.doesTypeMatches(pokemon.types, searchKey)) {
+        filteredList.push(pokemon);
+      }
+    });
+    return filteredList;
+  }
+
+  private doesTypeMatches(types: string[], searchKey): boolean {
+    let result = false;
+    types.forEach(type => {
+      result = result || type.search(searchKey) >= 0;
+    });
+    return result;
   }
 
 }
