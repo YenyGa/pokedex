75d5699fae08ba505731b9e220b0fdb0db4d1218
YenyGa <garcia.yeny@hotmail.com>
2020-02-09 23:09:50 -0500

    Adjusted password validator

--- src/app/core/validators/password.validator.ts
+++ src/app/core/validators/password.validator.ts
@@ -1,3 +1,3 @@
 import { ValidatorFn, Validators } from '@angular/forms';
 
-export const PasswordValidator: ValidatorFn = Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/);
+export const PasswordValidator: ValidatorFn = Validators.pattern(/^(?=.*[a-z])(?=.{2,}[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/);
